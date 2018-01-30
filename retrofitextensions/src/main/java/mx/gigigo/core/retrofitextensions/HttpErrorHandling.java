1fda9b77ce6054b2cf1080417ac6fc0064ca6d4e
Juan Godinez Vera <juan.godinez@gigigo.com.mx>
2018-01-29 18:33:27 -0600

    Se cambia HashMap por SparseArray, se remueven métodos, se refactoriza código

--- retrofitextensions/src/main/java/mx/gigigo/core/retrofitextensions/HttpErrorHandling.java
+++ retrofitextensions/src/main/java/mx/gigigo/core/retrofitextensions/HttpErrorHandling.java
@@ -2,41 +2,36 @@ package mx.gigigo.core.retrofitextensions;
 
 import android.content.Context;
 import android.support.annotation.StringRes;
+import android.util.SparseArray;
 
 import java.net.HttpURLConnection;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author VT - January 25, 2018.
  * @version 0.0.1
  * @since 0.0.1
  */
-public abstract class HttpErrorHandling{
+public abstract class HttpErrorHandling {
     public static final int DEFAULT_CODE = 0;
 
     private final Context context;
 
-    private Map<Integer, String> mapCodes;
+    private SparseArray<String> mapCodes;
 
-    public HttpErrorHandling(Context context){
+    public HttpErrorHandling(Context context) {
         this.context = context;
-        mapCodes = new HashMap<>();
+        mapCodes = new SparseArray<>();
 
-        try {
-            put(HttpURLConnection.HTTP_BAD_REQUEST, R.string.message_http_400);
-            put(HttpURLConnection.HTTP_UNAUTHORIZED, R.string.message_http_401);
-            put(HttpURLConnection.HTTP_FORBIDDEN, R.string.message_http_403);
-            put(HttpURLConnection.HTTP_NOT_FOUND, R.string.message_http_404);
-            put(HttpURLConnection.HTTP_BAD_METHOD, R.string.message_http_405);
-            put(HttpURLConnection.HTTP_BAD_GATEWAY, R.string.message_http_502);
-            put(DEFAULT_CODE, R.string.message_http_default);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        put(HttpURLConnection.HTTP_BAD_REQUEST, R.string.message_http_400);
+        put(HttpURLConnection.HTTP_UNAUTHORIZED, R.string.message_http_401);
+        put(HttpURLConnection.HTTP_FORBIDDEN, R.string.message_http_403);
+        put(HttpURLConnection.HTTP_NOT_FOUND, R.string.message_http_404);
+        put(HttpURLConnection.HTTP_BAD_METHOD, R.string.message_http_405);
+        put(HttpURLConnection.HTTP_BAD_GATEWAY, R.string.message_http_502);
+        put(DEFAULT_CODE, R.string.message_http_default);
     }
 
-    public void setMapCodes(HashMap<Integer, String> mapCodes) {
+    public void set(SparseArray<String> mapCodes) {
         this.mapCodes = mapCodes;
     }
 
@@ -55,17 +50,6 @@ public abstract class HttpErrorHandling{
     private String getStringFromId(Integer value) {
         return context.getString(value, "string", context.getPackageName());
     }
-
-
-    public Map<Integer, String> getMapCodes() {
-        return mapCodes;
-    }
-
-    public void setMapCodes(Map<Integer, String> mapCodes) {
-        this.mapCodes = mapCodes;
-    }
-
-
 }
 
 
