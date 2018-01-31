fad33d11f3104ed34eec3312621917e4e62373cb
Vianney <viannnne@gmail.com>
2018-01-30 18:24:42 -0600

    ANDCOMEX-13 in progress

--- app/src/main/java/mx/gigigo/core/data/repository/error/SimpleHandlerError.java
+++ app/src/main/java/mx/gigigo/core/data/repository/error/SimpleHandlerError.java
@@ -5,10 +5,10 @@ import com.google.gson.annotations.SerializedName;
 import mx.gigigo.core.rxmvp.ResponseError;
 
 /**
- * Created by Gigio on 25/01/18.
+ * Created by vt on 25/01/18.
  */
 
-public class SimpleHandlerError implements ResponseError{
+public class SimpleHandlerError implements ResponseError {
 
     @SerializedName("error")
     private String error;
